c47330ae007ba68916bc0f97c67cb75aac18c46c
Robert Perrotta <raaperrotta@gmail.com>
2016-08-23 18:52:31 -0400

    Added timer update function
    Added a calling syntax for statusbarTimer allowing the user to update
    the prefix of an existing timer. Added MATLAB File Exchange line to the
    help text. Added logic to gracefully handle closed figure (stops timer).

--- statusbarTimer.m
+++ statusbarTimer.m
@@ -1,48 +1,106 @@
-function t = statusbarTimer(varargin)
+function varargout = statusbarTimer(varargin)
 % STATUSBARTIMER Count-up timer for MATLAB status bar
 %
-%   t = STATUSBARTIMER([h],[prefix],) sets the MATLAB status bar to the prefix
-%   ("Busy..." if omitted or left blank) followed by a running clock of the
-%   elapsed time. STATUSBARTIMER uses a timer object named statusbarTimer
-%   to keep track of the elapsed time and update the status bar text. The
-%   function returns a handle to that timer object, which will delete
-%   itself and reset the status bar when stop(t) is called.
+%   t = STATUSBARTIMER([h],[prefix],[noprint]) sets the MATLAB status bar
+%   to the prefix ("Busy..." if omitted or left blank) followed by a
+%   running clock of the elapsed time. STATUSBARTIMER uses a timer object
+%   named statusbarTimer to keep track of the elapsed time and update the
+%   status bar text. The function returns a handle to that timer object,
+%   which will delete itself and reset the status bar when stop(t) is
+%   called. Unless noprint is set to true, the final time will be displayed
+%   in the command window in the style of toc.
+% 
+%   STATUSBARTIMER(t,prefix) updates the message string of an existing
+%   timer.
+% 
+% Examples:
+% 
+%   t = statusbarTimer();
+%   pause(5)
+%   stop(t)
+% 
+%   statusbarTimer(figure(),'Hello, World!')
+% 
+%   t = statusbarTimer('Entering loop');
+%   for ii = 1:10
+%       pause(1)
+%       statusbarTimer(t,sprintf('Finished iteration %d!',ii))
+%   end
+%   stop(t)
 %
+% Hosted on MATLAB File Exchange:
+%   <a href="matlab:system('open https://www.mathworks.com/matlabcentral/fileexchange/52833-statusbartimer');">https://www.mathworks.com/matlabcentral/fileexchange/52833-statusbartimer</a>
+% 
+% Requires:
+%   <a href="matlab:system('open https://www.mathworks.com/matlabcentral/fileexchange/52831-parsetime');">parseTime</a>
+%   <a href="matlab:system('open https://www.mathworks.com/matlabcentral/fileexchange/52832-num2sepstr');">num2sepstr</a>
+% 
 % Created by:
 %   Robert Perrotta
+
+narginchk(0,3)
+nargoutchk(0,1)
 
 h = 0;
 prefix = 'Busy...';
 noprint = false;
 
-if nargin == 1
-    if ishandle(varargin{1})
-        h = varargin{1};
-    else
-        prefix = varargin{1};
-    end
-elseif nargin > 1
-    h = varargin{1};
+n = nargin();
+
+if n==2 && isa(varargin{1},'timer') % Update UserData to be used by TimerFcn
+    t = varargin{1};
     prefix = varargin{2};
-    if nargin == 3
-        noprint = varargin{3};
+    data = get(t,'UserData');
+    data{1} = prefix;
+    set(t,'UserData',data)
+    return
+end
+
+if n >= 1
+    if isscalar(varargin{1}) && (varargin{1}==0 || ishghandle(varargin{1}))
+        h = varargin{1};
+        varargin = varargin(2:end);
+        n = n - 1;
+    end
+end
+
+if n == 2
+    prefix = varargin{2};
+    noprint = logical(varargin{3});
+elseif n == 1
+    if ischar(varargin{1})
+        prefix = varargin{1};
+    else
+        noprint = logical(varargin);
     end
 end
 
 existingTimer = timerfind('Name','statusbarTimer');
-if ~isempty(existingTimer)
-    stop(existingTimer)
-    delete(existingTimer)
+for ii = 1:length(existingTimer)
+    h2 = get(existingTimer(ii),'TimerFcn');
+    h2 = h2{end};
+    if h2 == h % existing timer applies to same target
+        stop(existingTimer(ii))
+    end
 end
 
 t = timer('Name','statusbarTimer','ExecutionMode','fixedRate','Period',0.07,...
     'TimerFcn',{@timerFcn,h},'UserData',{prefix,clock},'StopFcn',{@cleanup,h,noprint});
 start(t)
 
+if nargout == 1
+    varargout = {t};
+end
+
 end
 
 function timerFcn(t,~,h)
 
+if h~=0 && ~ishghandle(h) % Figure was closed
+    stop(t)
+    return
+end
+    
 data = get(t,'UserData');
 str = parseTime(etime(clock,data{2}));
 statusbar(h,sprintf('%s (%s elapsed.)',data{1},str))
@@ -58,6 +116,8 @@ if ~noprint
 end
 stop(t)
 delete(t)
-statusbar(h)
+if h==0 || ishghandle(h)
+    statusbar(h) % Clear status
+end
 
 end
